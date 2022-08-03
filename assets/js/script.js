a9e9bec234d1658bd0c268da8e29a3ad8d2bcd88
Fatih Rizqon <fatihrizqon@gmail.com>
2022-08-03 23:27:45 +0700

    Connecting with API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,54 @@
-const url = `http://127.0.0.1:8000/api/`
+const url = `https://api-pahlawankita/api/`
 const answer_button = document.querySelector("#answer_button");
 const question = document.querySelector("#question");
 const options = document.querySelector("#options");
 const results = document.querySelector("#results");
+const clue = document.querySelector("#clue");
 let section = 'home';
 let score = 0;
 let question_id;
 
-window.onload = function () {
+/* Navbar Effects */
+window.onscroll = () => {
+    const navbar = document.querySelector("#navbar");
+    const fixed = navbar.offsetTop;
+    if (window.pageYOffset > fixed) {
+        navbar.classList.add("navbar");
+        navbar.classList.remove("navbar-transparent");
+    } else {
+        navbar.classList.remove("navbar");
+        navbar.classList.add("navbar-transparent");
+    }
+};
+
+window.onload = () => {
     document.querySelector("#navigation") ? document.querySelector("#navigation").classList.remove("hidden") : '';
     getQuiz();
     getResult();
 }
 
 function index() {
-    console.log(score);
-    window.location = `http://127.0.0.1:5500/index.html`;
+    window.location = `https://fatihrizqon.github.io/pahlawankita`;
 }
 
 function getQuiz() {
+    // timer
     fetch(url + `quiz`)
         .then((response) => response.json())
         .then((response) => {
-            // console.log(response.data.options);
             question_id = response.data.question[0].id
+            clue.innerHTML =
+                `<div class="w-[200px] h-[200px] md:w-[250px] md:h-[250px] bg-cover rounded-full" style="background-image: url('https://direktoratk2krs.kemsos.go.id/admin-pc/assets/img/pahlawan/${ question_id }.jpg');"> </div>`
             question.innerHTML = `${ response.data.question[0].description }. Beliau lahir pada ${ response.data.question[0].date_of_birth } dan berasal dari ${ response.data.question[0].origin }. ${ response.data.question[0].name }`
             options.innerHTML = '';
             for (option of response.data.options) {
                 options.innerHTML += `<span class="flex items-center gap-x-2">
                     <input type="radio" id="answer" name="answer" value="${ option.id }">
-                    <label for="${ option.id }">${ option.id } - ${ option.name }</label>
+                    <label for="${ option.id }">${ option.name }</label>
                 </span>`
             }
         })
-        .catch((response) => console.log(response));
+        .catch((response) => console.log(response))
 }
 
 function getResult() {
@@ -52,15 +67,17 @@ function getResult() {
 }
 
 function checkAnswer() {
-    let answer = parseInt(document.querySelector('input[name="answer"]:checked').value);
-    if (answer === question_id) {
+    let answer = document.querySelector('input[name="answer"]:checked') ?? false;
+    if (answer.value === undefined) return alert("Please select an option.")
+
+    if (parseInt(answer.value) === question_id) {
         score += 1;
         alert(`Yes you are right! Your current score is ${ score }`);
         return getQuiz();
     } else {
-        alert(`Oops... you are picked a wrong answer.`);
+        alert(`[Quiz Over] Oops... you are picked a wrong answer.`);
         // change the min. score
-        if (score > 1) {
+        if (score > 10) {
             if (confirm(`Your score is ${ score }. Do you want to save this score ?`)) {
                 save(score)
             } else {
